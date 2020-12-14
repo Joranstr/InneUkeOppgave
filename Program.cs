80a8d4aed8b21692a5a032a28ab01bf14348587f
Joranstr <55440769+Joranstr@users.noreply.github.com>
2020-12-14 11:29:00 +0100

    1

--- Program.cs
+++ Program.cs
@@ -8,8 +8,29 @@ namespace InneUkeOppgave
         static void  Main(string[] args)
         {
             var toDos = new List<ToDo>();
-            toDos.Add(new ToDo("Middag", "Lag Middag", DateTime.Now.ToString("dd/mm/yy"), true));
+            for (int i = 0; i < 2; i++)
+            {
+                toDos = SetAsDone(toDos);
+            }
+            
+            
             TaskToDo.Show(toDos);
+        }
+
+        private static List<ToDo> SetAsDone(List<ToDo> toDos)
+        {
+            Console.WriteLine("Har du lagd middag? y/n");
+            var lagdmiddag =Console.ReadLine();
+            if (lagdmiddag == "y")
+            {
+                toDos.Add(new ToDo("Middag", "Lag Middag", DateTime.Now.ToString("dd/mm/yy"), true));
+            }
+            else
+            {
+                toDos.Add(new ToDo("Middag", "Lag Middag", DateTime.Now.ToString("dd/mm/yy"), false));
+            }
+
+            return toDos;
         }
     }
 }
