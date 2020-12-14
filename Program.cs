d74514f09eba07b6d5213ecc29102dfbc4e542e1
Joranstr <55440769+Joranstr@users.noreply.github.com>
2020-12-14 10:47:40 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InneUkeOppgave
+{
+    class Program
+    {
+        static void  Main(string[] args)
+        {
+            var toDos = new List<ToDo>();
+            toDos.Add(new ToDo("Middag", "Lag Middag", DateTime.Now.ToString("dd/mm/yy"), true));
+            TaskToDo.Show(toDos);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TaskToDo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using static InneUkeOppgave.ToDo;
+
+namespace InneUkeOppgave
+{
+    public class TaskToDo
+    {
+        
+        private readonly string _taskName;
+        private string _description;
+        private DateTime _duoDate;
+
+        public TaskToDo(string taskName, string description, DateTime duoDate)
+        {
+            _taskName = taskName;
+            _description = description;
+            _duoDate = duoDate;
+        }
+
+        public static void Show(List<ToDo>toDos)
+        {
+            WritHeader();
+            foreach (var toDo in toDos)
+            {
+                if (toDo.IsDone)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                
+                Console.Write(toDo.TaskName+"; ");
+                Console.Write(toDo.Description+"; ");
+                Console.Write(toDo.DuoDate+"; ");
+                Console.Write(toDo.IsDone);
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            
+        }
+
+        private static void WritHeader()
+        {
+            Console.Write("Name of Task; ");
+            Console.Write("Description; ");
+            Console.Write("Do Date; ");
+            Console.Write("Is it Done; ");
+            Console.WriteLine();
+        }
+    }
+}
