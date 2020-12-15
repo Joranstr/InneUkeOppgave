6dddde06089fce5016825d79517bac9e3c375e66
Joranstr <55440769+Joranstr@users.noreply.github.com>
2020-12-15 09:44:10 +0100

    1

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ namespace InneUkeOppgave
         static void  Main(string[] args)
         {
             var toDos = new List<TaskToDo>();
-            toDos.Add($"Lag Middag;", true);
-            var toDo2 = ($"Lag Middag;", true);
-            TaskToDo.Show(toDo);
+            toDos.Add(new TaskToDo($"Lag Middag;", true));
+            toDos.Add(new TaskToDo($"Lag Middag;", false));
+            TaskToDo.Show(toDos);
         }
     }
 }
--- TaskToDo.cs
+++ TaskToDo.cs
@@ -1,23 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace InneUkeOppgave
 {
     public class TaskToDo
     {
         private string _beskrivelse;
-        private string _isDone;
-        
-        public TaskToDo(string beskrivelse, string isDone)
+        private bool _isDone;
+
+        public TaskToDo(string beskrivelse, bool isDone)
         {
             _beskrivelse = beskrivelse;
             _isDone = isDone;
         }
-        public static void Show( string toDo)
+
+        public static void Show(List<TaskToDo> toDos)
         {
-            // WritHeader();
-            // Console.ForegroundColor= ConsoleColor.Green;
-            Console.WriteLine(toDo);
-            
+            foreach (var toDo in toDos)
+            {
+                SetAsDone(toDo);
+                Console.WriteLine(toDo);
+            }
+        }
+
+        private static void SetAsDone(TaskToDo toDo)
+        {
+            if (toDo._isDone)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
         }
 
         private static void WritHeader()
